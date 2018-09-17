0b885211b6ee326bd14447bde9f28a2de263b0c0
Liam Logan-Davies <liamlogan16@yahoo.co.uk>
2018-09-17 17:55:50 +0100

    Implemented very simple console app to read & write to a txt file

--- SSD-CA1/Program.cs
+++ SSD-CA1/Program.cs
@@ -11,6 +11,63 @@ namespace SSD_CA1
     {
         static void Main(string[] args)
         {
+            string input="";
+            try
+            {
+                do
+                {
+                    Console.Clear();
+                    input = GetData(input);
+                } while (input != "");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        private static string GetData(string input)
+        {
+            Console.WriteLine("Enrolled Students:");
+            StreamReader sr = new StreamReader("Data.txt");
+            input = sr.ReadLine();
+            while (input != null && input != "")
+            {
+                Console.WriteLine(input);
+                input = sr.ReadLine();
+            }
+            sr.Close();
+            input = CheckInput(input);
+            return input;
+        }
+
+        private static string CheckInput(string input)
+        {
+            input = "";
+            while (input.ToUpper() != "Y" && input.ToUpper() != "N")
+            {
+                Console.WriteLine("Would you like to add a student?(Y/N)");
+                input = Console.ReadLine();
+                if (input.ToUpper() == "Y") {
+                    AddStudent();
+                }
+                else if (input.ToUpper() == "N")
+                {
+                    input = "";
+                    break;
+                }
+            }
+            return input;
+        }
+
+        private static void AddStudent()
+        {
+            Console.WriteLine("Enter the student's name");
+            string input = Console.ReadLine();
+            StreamWriter sw = new StreamWriter("Data.txt",true);
+            sw.WriteLine(input);
+            sw.Close();
         }
     }
 }
